ab58cb35ba730028c62122fe2204c4d2b1acb18f
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-26 14:20:26 +0700

    feat : fetch API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import money from "./image/money.png";
 import "./App.css";
 import CurrencyComponent from "./components/CurrencyComponent";
+import { useEffect } from "react";
 
 function App() {
+  const url = `https://api.exchangerate-api.com/v4/latest/USD`;
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div>
-      <img src={money} alt="logo" className='money-img' />
+      <img src={money} alt="logo" className="money-img" />
       <h1>Currency Exchange (API)</h1>
       <div className="container">
         <CurrencyComponent />
-        <div className='equal'> = </div>
+        <div className="equal"> = </div>
         <CurrencyComponent />
       </div>
     </div>
